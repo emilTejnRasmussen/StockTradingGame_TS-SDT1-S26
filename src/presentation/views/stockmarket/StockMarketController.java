d48aca01a9dc54b49fd819f13879f32994ec4511
Emil Tejn Rasmussen <emil@rasie.dk>
2026-03-27 19:49:18 +0100

    starting on buy table

--- src/presentation/views/stockmarket/StockMarketController.java
+++ src/presentation/views/stockmarket/StockMarketController.java
@@ -1,14 +1,31 @@
 package presentation.views.stockmarket;
 
+import business.dto.StockDTO;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.*;
 import javafx.util.Duration;
+
+import java.math.BigDecimal;
 
 public class StockMarketController
 {
+
+
+    @FXML
+    private TableView<StockDTO> stockTableView;
+    @FXML
+    private TableColumn<StockDTO, String> symbolCol;
+    @FXML
+    private TableColumn<StockDTO, BigDecimal> priceCol;
+    @FXML
+    private TableColumn<StockDTO, Void> actionCol;
+
     @FXML
     private LineChart<Number, Number> stockMarketChart;
 
@@ -28,6 +45,34 @@ public class StockMarketController
 
     @FXML
     public void initialize()
+    {
+        setupLineChart();
+        setupTableView();
+
+    }
+
+    private void setupTableView()
+    {
+        stockTableView = new TableView<>();
+        symbolCol = new TableColumn<>("Symbol");
+        symbolCol.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().symbol()));
+
+        priceCol = new TableColumn<>("Price");
+        priceCol.setCellValueFactory(data -> new SimpleObjectProperty<>(data.getValue().currentPrice()));
+
+        actionCol = new TableColumn<>("");
+        actionCol.setCellFactory(param -> new TableCell<>() {
+            private final Button buyButton = new Button("Buy");
+            {
+                buyButton.setOnAction(event -> {
+                    StockDTO stock = getTableView().getItems().get(getIndex());
+                    System.out.println("Buying: " + stock.symbol());
+                });
+            }
+        });
+    }
+
+    private void setupLineChart()
     {
         xAxis.setLabel("Time (seconds)");
         yAxis.setLabel("Price");
